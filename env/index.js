f797ba40a0c6295c1193127aa95743b2b138cac9
Jishnu <jishnu.nb@neoito.com>
2022-05-03 19:51:36 +0530

    debug fix

--- env/index.js
+++ env/index.js
@@ -1,43 +1,43 @@
-import dotenv from "dotenv";
-import path from "path";
+// import dotenv from "dotenv";
+// import path from "path";
 
-// Path to root directory env
-const getEnvPath = (options) => {
-  const { dir, envFileName, relative } = options;
+// // Path to root directory env
+// const getEnvPath = (options) => {
+//   const { dir, envFileName, relative } = options;
 
-  const root = `${relative}/`
-    .split(dir)[1]
-    .replace(/(?<=\/)(.*?)(?=\/)/g, "..");
+//   const root = `${relative}/`
+//     .split(dir)[1]
+//     .replace(/(?<=\/)(.*?)(?=\/)/g, "..");
 
-  return `${relative}${root || ""}../${envFileName}`;
-};
+//   return `${relative}${root || ""}../${envFileName}`;
+// };
 
-// Setup environment
-const init = () => {
-  let relative = path.resolve();
-  let envPath = `${relative}/.env`;
+// // Setup environment
+// const init = () => {
+//   let relative = path.resolve();
+//   let envPath = `${relative}/.env`;
 
-  [
-    {
-      dir: "functions",
-      envFileName: ".env.functions",
-    },
-    {
-      dir: "._ab_em",
-      envFileName: ".env.functions",
-    },
-    {
-      dir: "view",
-      envFileName: ".env.view",
-    },
-  ].some((pathData) => {
-    if (!envPath.includes(`/${pathData.dir}`)) return false;
+//   [
+//     {
+//       dir: "functions",
+//       envFileName: ".env.functions",
+//     },
+//     {
+//       dir: "._ab_em",
+//       envFileName: ".env.functions",
+//     },
+//     {
+//       dir: "view",
+//       envFileName: ".env.view",
+//     },
+//   ].some((pathData) => {
+//     if (!envPath.includes(`/${pathData.dir}`)) return false;
 
-    envPath = getEnvPath({ ...pathData, relative });
-    return true;
-  });
+//     envPath = getEnvPath({ ...pathData, relative });
+//     return true;
+//   });
 
-  dotenv.config({ path: envPath });
-};
+//   dotenv.config({ path: envPath });
+// };
 
-export default { init };
+// export default { init };
